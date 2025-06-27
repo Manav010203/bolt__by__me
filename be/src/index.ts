3d85b3fdc5b4ce75d7e32920deff64bba3946beb
Manav010203 <manav01022003@gmail.com>
2025-06-27 18:39:28 +0530

    added stream output to the app

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,3 +1,34 @@
-require("dotenv").config();
+import { GoogleGenerativeAI } from '@google/generative-ai';
+const API_KEY = 'AIzaSyDaEXVeA07xeA3T5ypMG2z9yKY6A76N_h8'; 
+async function runGeminiExample() {
 
-console.log(process.env.CLAUDE_API_KEY)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+
+  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
+
+  const prompt = "tell me about ai in 100 words";
+
+  console.log("Sending prompt to Gemini API...");
+  console.log(`Prompt: "${prompt}"`);
+
+  try {
+    const result = await model.generateContentStream(prompt);
+    
+
+    console.log("\n--- Gemini API Response ---");
+    for await(const chunk of result.stream){
+        console.log(chunk.text()||' ');
+    }
+    
+    console.log("---------------------------\n");
+
+  } catch (error) {
+    console.error("Error calling Gemini API:", error);
+    if (error instanceof Error) {
+      console.error("Error message:", error.message);
+    }
+  }
+}
+runGeminiExample();
+
+
